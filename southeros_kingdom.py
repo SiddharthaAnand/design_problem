9e0e87cf1f3a1e7353c52c3a8c0cab249c3b509d
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-05 19:30:35 +0530

    Create skeleton for Geektrust Problem 1: Backend Challenge

---
+++ southeros_kingdom.py
@@ -0,0 +1,21 @@
+class Kingdom(object):
+
+	def __init__(self, name, emblem):
+		self.name = name
+		self.emblem = emblem
+
+	def get_name(self):
+		pass
+
+	def get_emblem(self):
+		pass
+
+	def receive_msg(self, secret_message):
+		pass
+
+	def decipher_msg(self, secret_message):
+		pass
+
+	def send_reply(self):
+		pass
+		
---
+++ universe.py
@@ -0,0 +1,17 @@
+class Universe(object):
+
+	def __init__(self, ruler):
+		self.ruler = ruler
+
+	def current_ruler(self):
+		pass
+
+	def current_ruler_emblem(self):
+		pass
+
+	def total_kingdoms(self):
+		pass
+
+	
+
+
