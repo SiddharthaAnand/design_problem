b57a32b6682b8f1770f63445ea130f4804b16687
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-15 20:35:55 +0530

    Add methods for kingdoms

--- src/southeros_kingdom.py
+++ src/southeros_kingdom.py
@@ -1,21 +1,27 @@
+import general_store
+
 class Kingdom(object):
 
 	def __init__(self, name, emblem):
 		self.name = name
 		self.emblem = emblem
+		self.secret_message = None
+		self.deciphered_message = None
+		self.reply = None
+		self.toolbox = general_store.GeneralStore()
 
 	def get_name(self):
-		pass
+		return self.name
 
 	def get_emblem(self):
-		pass
+		return self.emblem
 
 	def receive_msg(self, secret_message):
-		pass
+		self.secret_message = secret_message
 
 	def decipher_msg(self, secret_message):
-		pass
+		self.reply = self.toolbox.message_decoder(secret_message)
 
 	def send_reply(self):
-		pass
+		return self.reply 
 		
