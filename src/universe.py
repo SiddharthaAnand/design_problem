77306b24e07b36eb4a5c58ab427670937f732951
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-16 21:12:20 +0530

    Update universe code structure

--- src/universe.py
+++ src/universe.py
@@ -1,17 +1,32 @@
+import southeros_kingdom
+
 class Universe(object):
 
-	def __init__(self, ruler):
-		self.ruler = ruler
+	def __init__(self, kingdoms):
+		self.kingdoms = kingdoms
+		self.ruler = None
 
 	def current_ruler(self):
-		pass
+		return self.ruler
 
 	def current_ruler_emblem(self):
-		pass
+		return self.ruler.kingdom.emblem
 
 	def total_kingdoms(self):
 		pass
 
+	def create_kingdom(self, name, emblem):
+		
+	def run_it(self):
+
 	
+if __name__ == '__main__':
+	universe = Universe({"LAND": "PANDA", 
+							"WATER": "OCTOPUS", 
+							"ICE":"MAMMOTH", 
+							"AIR":"OWL", 
+							"FIRE": "DRAGON", 
+							"SPACE":"GORILLA"
+							})
 
 
